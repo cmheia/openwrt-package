bcd71f01cd83c96003b2d4d71733f6cde7b99afd
cmheia <cmheia@gmail.com>
2020-03-19 23:12:40 +0800

    3proxy: fix add3proxyuser.sh

--- 3proxy/files/add3proxyuser.sh
+++ 3proxy/files/add3proxyuser.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ensure_file_exist () {
+ensure_file_exist() {
 	[ -f "$1" ] || touch "$1"
 }
 
@@ -12,9 +12,9 @@ if [ $4 ]; then
 	ensure_file_exist $PWD_CFG
 	ensure_file_exist $CNT_CFG
 	ensure_file_exist $BAND_CFG
-	echo $1:`/usr/bin/mycrypt $$ $2` >> $PWD_CFG
-	echo countin \"`wc -l $CNT_CFG|awk '{print $1}'`/$1\" D $3 $1 >> $CNT_CFG
-	echo bandlimin $4 $1 >> $BAND_CFG
+	echo $1:$(/usr/bin/mycrypt $$ $2) >>$PWD_CFG
+	echo countin \"$(wc -l $CNT_CFG | awk '{print $1}')/$1\" D $3 $1 >>$CNT_CFG
+	echo bandlimin $4 $1 >>$BAND_CFG
 else
 	echo usage: $0 username password day_limit bandwidth
 	echo "	"day_limit - traffic limit in MB per day
